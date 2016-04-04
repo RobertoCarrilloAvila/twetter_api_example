7b3f57e38801df40809c71e2f6d62e8d0a583a06
roberto <roberto@roberto-Aspire-5745.(none)>
2016-04-04 13:39:07 -0500

    peticion ajax lista

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,8 +10,29 @@ post '/add' do
 		#puts "*"*30
 		#puts "valido"
 	end
+	
 	redirect to "/#{u.name}"
 end
+
+
+
+get '/tweet' do
+    erb :new_tweet
+end
+
+
+post '/tweet' do
+	text = params[:texto]
+    #begin
+    	puts "*"*40
+    	puts text
+    	CLIENT.update(text)
+    	erb "Twett publicado exitosamente", layout: false
+    #rescue
+    #	erb "Error al publicar el Twett", layout: false
+    #end
+end
+
 
 get '/:user' do
 	@name = params[:user]
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,32 @@
 $(document).ready(function() {
-	// Este código corre después de que `document` fue cargado(loaded) 
-	// completamente. 
-	// Esto garantiza que si amarramos(bind) una función a un elemento 
-	// de HTML este exista ya en la página. 
+
+	$(".loading").hide();
+
+	$("#formulario").submit(function() {
+  		$(".other").css("opacity", "0.4")
+		$(".loading").show();
+	});
+
+
+
+	$("#twettear").submit(function( event ) {
+		event.preventDefault();
+		var variables = $("#twettear").serialize();
+  		$(".other").css("opacity", "0.4");
+		$(".loading").show();
+		$("#twettear > *").prop('disabled', true);
+		//console.log($("#twettear > *"));
+		//console.log("formulario");
+		//console.log(variables);
+		$.post('/tweet', variables, function(resp) {
+			//console.log(resp);
+			$("#result").html("<h4>"+resp+"</h4>");
+			$("#twettear > *").prop('disabled', false);
+			$(".other").css("opacity", "1");
+			$(".loading").hide();
+			$("#area").val("");
+		});
+
+	});
 
 });
